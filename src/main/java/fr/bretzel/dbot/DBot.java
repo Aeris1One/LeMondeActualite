bc924c6db860f1b6bc71470d999c3f3547066899
MrBretzel <axelolo68@gmail.com>
2022-03-01 01:23:02 +0100

    Refresh Rate config

--- src/main/java/fr/bretzel/dbot/DBot.java
+++ src/main/java/fr/bretzel/dbot/DBot.java
@@ -3,6 +3,7 @@ package fr.bretzel.dbot;
 import com.google.gson.JsonArray;
 import fr.bretzel.config.Config;
 import fr.bretzel.config.entry.JsonElementEntry;
+import fr.bretzel.config.entry.NumberEntry;
 import fr.bretzel.config.entry.StringEntry;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -14,6 +15,7 @@ public class DBot {
 
     public static final StringEntry DISCORD_TOKEN = new StringEntry("token", "");
     public static final JsonElementEntry DISCORD_CHANNEL_ID = new JsonElementEntry("channel_ids", new JsonArray());
+    public static final NumberEntry REFRESH_TIMING = new NumberEntry("Refresh Rate (in minute)", 1);
 
     public static final StringEntry LAST_CHECKED_URL = new StringEntry("last checked url", "https://www.lemonde.fr/international/live/2022/02/27/guerre-en-ukraine-en-direct-l-union-europeenne-va-armer-kiev-les-occidentaux-appellent-leurs-ressortissants-a-quitter-la-russie_6115418_3210.html");
     public static final StringEntry LAST_TITLE = new StringEntry("last title", "");
@@ -28,6 +30,7 @@ public class DBot {
             config.write();
             config.getOrAdd(DISCORD_TOKEN);
             config.getOrAdd(DISCORD_CHANNEL_ID);
+            config.getOrAdd(REFRESH_TIMING);
         }
     });
 
@@ -59,7 +62,7 @@ public class DBot {
         System.out.println("[DBOT]: Starting timer for checking site LeMonde.fr");
 
         LiveChecker checker = new LiveChecker();
-        new Timer().schedule(checker, new Date(), (1000 * 60));
+        new Timer().schedule(checker, new Date(), (1000 * (60L * mainConfig.getOrAdd(REFRESH_TIMING).longValue())));
 
     }
 }
